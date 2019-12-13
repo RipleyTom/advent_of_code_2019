f2882ec4a8059239b4c33677e78971713118a351
RipleyTom <RipleyTom@users.noreply.github.com>
2019-12-13 15:45:08 +0100

    Day13

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #[allow(non_snake_case)]
-mod Day12;
+mod Day13;
 
 fn main() {
-    Day12::part1::run_puzzle();
-    Day12::part2::run_puzzle();
+    Day13::part1::run_puzzle();
+    Day13::part2::run_puzzle();
 }
